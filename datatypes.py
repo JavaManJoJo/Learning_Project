4f0f8489e457ac8b4c52e46589a04a96f87e0244
JavaManJoJo <javamanjojo@gmail.com>
2016-06-29 15:01:08 -0700

    Learned about datatypes and build systems

---
+++ datatypes.py
@@ -0,0 +1,53 @@
+#primitive data types
+
+#Integers
+3
+2
+1
+-9
+
+#floats / singles
+
+2.1
+4.5
+-3.14
+
+#doubles 64 bits
+
+#characters
+'a'
+'b'
+'c'
+
+#Strings
+"Cat"
+"dog"
+
+#Bool / BooLean
+isAtHome = True
+hasMoney = False
+
+
+# high level
+# Python
+
+# C based lanagues
+#java, C++, C#
+
+# C level lanauges
+#main()
+#{
+#	int num = 5;
+#	printf("Hello World\n");
+#	return 0;
+#} 
+
+# Assembly
+# it is not multiply platform
+# mov eax, 5
+
+#Byte code
+11101001 11011011
+
+#Binary 
+0000111100101010101010101011111010000101010
--- source.py
+++ source.py
@@ -1,1 +1,11 @@
-print ("hello")
+def main():
+	# Gets the user's name and stores it in a userName
+	userName = input("Username: ")
+
+	# Gets the user input and change it to a string and then stores it in user ago
+	userAge = int(input("Age: "))
+
+	print("Hello, " + userName + ", your are " + str(userAge) + " years old.")
+
+something
+main()
